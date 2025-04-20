9008408eed11028358dbc43ac2b66659521fced4
Dennis <pozgaydennis@gmail.com>
2025-04-20 11:39:06 +0400

    Added First Problem Solution
    This is the first problem I solved, the solution is perhaps not the best, but I want to document exactly what i did and how I did it, even if it's not the most effective to hopefully see progress with time.

---
+++ twosum.py
@@ -0,0 +1,27 @@
+nums = [1,3,3]
+target = 6
+result = []
+found = False
+
+while not found:
+    for num in nums:
+        nums2 = nums.copy()
+        nums2.remove(num)
+        for num2 in nums2:
+            if num2 + num == target:
+                id1 = nums.index(num)
+                result.append(id1)
+                templist = nums[id1+1:]
+                id2 = id1 + 1 + templist.index(num2) if num == num2 else nums.index(num2)
+
+                result.append(id2)
+                found = True
+                break
+        if found:
+            break
+        
+
+
+print(result)
+
+enumerate       
