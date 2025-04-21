ae70b86ad1d61b4bc3bb109eb8370eb5d898ef71
Dennis <pozgaydennis@gmail.com>
2025-04-21 10:57:47 +0400

    Edited solution format

--- addtwonumbers.py
+++ addtwonumbers.py
@@ -1,49 +1,36 @@
 # Problem 2
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val      # Stores a single digit (0-9)
-        self.next = next    # Points to the next node
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        number1 = ''
+        number2 = ''
+
+        current = l1
+        current2 = l2
+        while current or current2:
+            if current:
+                number1 += str(current.val)
+                current = current.next
+            if current2:
+                number2 += str(current2.val)
+                current2 = current2.next
 
 
-l12 = ListNode(3)
-l11 = ListNode(4, l12)
-l1 = ListNode(2, l11)
+        number1 = number1[::-1]
+        number2 = number2[::-1]
+        total = int(number1) + int(number2)
 
-l22 = ListNode(4)
-l21 = ListNode(6, l22)
-l2 = ListNode(5, l21)
+        list1 = list(str(total))
+        list1.reverse()
 
-number1 = ''
-number2 = ''
+        listfinal = [int(y) for y in list1]
+        
+        dum = ListNode(0)
+        current = dum
 
-current = l1
-current2 = l2
-while current:
-    number2 += str(current2.val)
-    number1 += str(current.val)
-    current = current.next
-    current2 = current2.next
+        for digit in listfinal:
+            current.next = ListNode(digit)
+            current = current.next
 
-number1 = number1[::-1]
-number2 = number2[::-1]
-total = int(number1) + int(number2)
-
-list1 = list(str(total))
-list1.reverse()
-
-listfinal = [int(y) for y in list1]
-print(listfinal)
-
-
-
-
-dum = ListNode(0)
-current = dum
-
-for digit in listfinal:
-    current.next = ListNode(digit)
-    current = current.next
-
-result = dum.next
-print(result)
+        result = dum.next
+        return result
