c3ea092264f649776c0c8bbceb195f121c21767c
Dennis <pozgaydennis@gmail.com>
2025-07-02 10:51:34 +0200

    added problem file - no solution

---
+++ Easy/Problem228.py
@@ -0,0 +1,14 @@
+# Problem 228 - Summary Ranges
+
+nums = [0,1,2,4,5,7]
+outp = []
+# for i in range(len(nums)):
+#     if nums[i+1] == nums[i]+1:
+#         pass
+#     else:
+#         outp.append(f'{nums[i]}')
+ 
+
+for i in range(len(nums)):
+    if nums[i+1] != nums[i]+1:
+        pass
